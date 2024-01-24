fee52260204017b1bd76692de08e974a33f291cd
ak-3835 <55884023+ak-3835@users.noreply.github.com>
2024-01-24 16:43:27 +0500

    Comment code deleted

--- ikonic/template-parts/home-page/frequesntly-asked-questions.php
+++ ikonic/template-parts/home-page/frequesntly-asked-questions.php
@@ -33,36 +33,4 @@
     echo "nothing found";
     endif;
     ?>
-    
-
-    <!-- Accordion Item 2 -->
-    <!-- <div class="card mt-3">
-        <div class="card-header" id="headingTwo" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-            <h5 class="mb-0"><?php //echo $questions['question_2']; ?></h5>
-        </div>
-
-        <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
-            <div class="card-body"><?php //echo $questions['answer_2']; ?></div>
-        </div>
-    </div>
-
-    <div class="card mt-3">
-        <div class="card-header" id="headingThree" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-            <h5 class="mb-0"><?php //echo $questions['question_3']; ?></h5>
-        </div>
-
-        <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
-            <div class="card-body"><?php //echo $questions['answer_3']; ?></div>
-        </div>
-    </div>
-
-    <div class="card mt-3">
-        <div class="card-header" id="headingFour" data-toggle="collapse" data-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
-            <h5 class="mb-0"><?php //echo $questions['question_4']; ?></h5>
-        </div>
-
-        <div id="collapseFour" class="collapse" aria-labelledby="headingFour" data-parent="#accordionExample">
-            <div class="card-body"><?php //echo $questions['answer_4']; ?></div>
-        </div>
-    </div> -->
 </div>
